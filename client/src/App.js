1c9944e8f99736268e5d931d9f7bb8b09cbc62a9
Anymokhtar <86941457+Anymokhtar@users.noreply.github.com>
2023-12-07 03:41:22 +0100

    Login_Logout_Functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import HomePage from "./pages/home/HomePage";
 import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 import RegisterPage from "./pages/register/RegisterPage";
+import LoginPage from "./pages/login/LoginPage";
 import { Toaster } from "react-hot-toast";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route index path="/" element={<HomePage />} />
         <Route path="/blog/:slug" element={<ArticleDetailPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
       <Toaster/>
     </div>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { images } from "../constants";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../store/actions/user";
+import { useNavigate } from "react-router-dom";
+
 const navItemsInfo = [
   { name: "Home", type: "link" },
   { name: "Articles", type: "link" },
@@ -61,12 +65,22 @@ const NavItem = ({ item }) => {
   );
 }
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [navIsVisible, setNavIsVisible] = useState(false);
+  const userState = useSelector(state => state.user);
+  const [profileDrowpdown, setProfileDrowpdown] = useState(false);
+
   const navVisibilityHandler = () => {
     setNavIsVisible((curState) => {
       return !curState;
     });
   };
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <section className="sticky top-0 left-0 right-0 z-50 bg-white">
       <header className='container mx-auto px-5 flex justify-between py-4 items-center'>
@@ -92,9 +106,43 @@ const Header = () => {
               <NavItem key={item.name} item={item} />
             )}
           </ul>
-          <button className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
-            Sign In
-          </button>
+          {userState.userInfo ? (
+            <div className='text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold'>
+              <div className="relative group">
+                <div className="flex flex-col items-center">
+                  <button className="flex gap-x-1 items-center mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300"
+                    onClick={() => setProfileDrowpdown(!profileDrowpdown)}>
+                    <span>Profile</span>
+                    <MdKeyboardArrowDown />
+                  </button>
+                  <div className={`${profileDrowpdown ? "block" : "hidden"} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}>
+                    <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
+                      <button
+                        type="button"
+                        className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft">
+                        Dashboard
+                      </button>
+                      <button
+                        onClick={logoutHandler}
+                        type="button"
+                        className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft">
+                        Logout
+                      </button>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          ) : (
+            <button
+              onClick={() => navigate("/login")}
+              className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300"
+            >
+              Sign in
+            </button>
+          )}
+
         </div>
       </header>
     </section>
