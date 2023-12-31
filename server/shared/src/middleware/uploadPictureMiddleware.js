831c59a82904929c3af3882771f886bcf71ea554
Anymokhtar <86941457+Anymokhtar@users.noreply.github.com>
2023-12-31 16:37:00 +0100

    Security Issues

--- server/shared/src/middleware/uploadPictureMiddleware.js
+++ server/shared/src/middleware/uploadPictureMiddleware.js
@@ -3,32 +3,25 @@ import path from "path";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "../uploads"));
+        cb(null, path.join(__dirname, "../../../uploads"));
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`);
     },
 });
 
-const limits = {
-    fileSize: {
-        images: 8 * 1024 * 1024, // 8MB for image uploads
-        others: 2 * 1024 * 1024, // 2MB for other requests
-    },
-};
-
-const fileFilter = (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
-        return cb(new Error("Only images are allowed"));
-    }
-    cb(null, true);
-};
-
 const uploadPicture = multer({
     storage: storage,
-    limits: { fileSize: limits.fileSize.images },
-    fileFilter: fileFilter,
+    limits: {
+        fileSize: 1 * 2000000, // 2MB
+    },
+    fileFilter: function (req, file, cb) {
+        let ext = path.extname(file.originalname);
+        if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
+            return cb(new Error("Only images are allowed"));
+        }
+        cb(null, true);
+    },
 });
 
 export { uploadPicture };
